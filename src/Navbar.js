80b908a9f642bb0071e4e7b3007c7ca628fc1f2f
almonasterio <a.monasterio.a@gmail.com>
2020-04-28 14:57:34 -0500

    select implemented

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react'
+import Select from '@material-ui/core/Select';
 import 'rc-slider/assets/index.css'
 import Slider from 'rc-slider'
 import "./Navbar.css"
+import MenuItem from '@material-ui/core/MenuItem';
 
 export default class Navbar extends Component {
+constructor(props){
+    super(props);
+    this.state = {format:"hex"}
+    this.handleChange=this.handleChange.bind(this)
+}
+handleChange(e){
+    this.setState({
+        format: e.target.value
+    })
+    this.props.handleChange(e.target.value)
+
+}
+
     render() {
+        const{level,changeLevel, handleChange} = this.props;
+        const {format} =this.state;
         return (
         <header className="Navbar">
             <div className="logo">
                 <a href='#'>reactcolorpicker</a>
             </div>          
             <div className="slider-container">
-            <span>Level: {this.props.level}</span>
+            <span>Level: {level}</span>
                 <div className = "slider" >
                     <Slider 
-                    defaultValue={this.props.level} 
+                    defaultValue={level} 
                     min={100} 
                     max={900} 
-                    onAfterChange={this.props.changeLevel} 
+                    onAfterChange={changeLevel} 
                     step={100}
                 />
                 </div>
+            </div>
+            <div className="select-container">
+            <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgb(255,255,255,1)</MenuItem>
+            </Select>
             </div>
          </header>
         )
--- src/Palette.js
+++ src/Palette.js
@@ -6,21 +6,25 @@ import NavBar from './Navbar'
 class Palette extends Component {
     constructor(props){
         super(props);
-        this.state={level:500}
+        this.state={level:500,format: "hex"}
         this.changeLevel=this.changeLevel.bind(this)
+        this.changeFormat=this.changeFormat.bind(this)
     }
     changeLevel(level){
         this.setState({level})
     }
+    changeFormat(value) {
+       this.setState({format:value})
+    }
     
     render() {
         const {colors} = this.props.palette;
-        const {level} = this.state;
-        const colorBoxes = colors[level].map(c => <ColorBox background={c.hex} name={c.name}></ColorBox>)
+        const {level,format } = this.state;
+        const colorBoxes = colors[level].map(c => <ColorBox background={c[format]} name={c.name}></ColorBox>)
 
         return (
             <div className="Palette">
-            <NavBar level={level} changeLevel={this.changeLevel}></NavBar>
+            <NavBar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}></NavBar>
                 <div div className = "Palette-colors" >
                     {colorBoxes}
                {/* A bunch of colors */}
