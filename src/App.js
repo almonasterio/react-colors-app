b6d9ccbdcbd0058c362ca321230f3134295b6d14
almonasterio <a.monasterio.a@gmail.com>
2020-04-28 15:55:00 -0500

    Routee implemented

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Palette from './Palette'
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelpers'
 import React, { Component } from 'react'
+import {Route, Switch} from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     console.log(generatePalette(seedColors[4]))
     return (
-      <div>
+      <Switch>
+        <Route exact path='/' render={()=> <h1>PALETTE LIST GOES HERE</h1>}/>
+        <Route exact path='/palette:id' render={()=> <h1>Individual Palette</h1>}/>
+      </Switch>
+      /* <div>
          <Palette palette={generatePalette(seedColors[4])}/>
-      </div>
+      </div> */
     )
   }
 }
