893960cd0105c04c6c646a04449715ffc0cb0590
almonasterio <a.monasterio.a@gmail.com>
2020-04-30 09:38:21 -0500

    added palette name validator

--- src/App.js
+++ src/App.js
@@ -8,30 +8,34 @@ import NewPaletteForm from "./NewPaletteForm";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
-constructor(props){
-  super(props);
-  this.state={
-    palettes:seedColors,
+  constructor(props) {
+    super(props);
+    this.state = { palettes: seedColors };
+    this.savePalette = this.savePalette.bind(this);
+    this.findPalette = this.findPalette.bind(this);
   }
-  this.savePalette = this.savePalette.bind(this)
-  this.findPalette=this.findPalette.bind(this)
-}
-
   findPalette(id) {
     return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
   }
-
   savePalette(newPalette) {
-this.setState({
-  palettes: [...this.state.palettes, newPalette]
-})
+    this.setState({ palettes: [...this.state.palettes, newPalette] });
   }
   render() {
     return (
       <Switch>
-        <Route exact path='/palette/new' render={(routeProps) => <NewPaletteForm {...routeProps} savePalette={this.savePalette}/>} />
+        <Route
+          exact
+          path='/palette/new'
+          render={routeProps => (
+            <NewPaletteForm
+              savePalette={this.savePalette}
+              palettes={this.state.palettes}
+              {...routeProps}
+            />
+          )}
+        />
         <Route
           exact
           path='/palette/:paletteId/:colorId'
