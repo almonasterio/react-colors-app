460e7a3a66ae5df6a455179b8ad5be40fe8b330a
almonasterio <a.monasterio.a@gmail.com>
2020-04-29 19:21:02 -0500

    add new palette form

---
+++ src/NewPaletteForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewPaletteForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>FORM</h1>
+            </div>
+        )
+    }
+}
