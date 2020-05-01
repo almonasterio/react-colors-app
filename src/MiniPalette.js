347c99ca49a1c5342b68667d67dfb57ef17e9481
almonasterio <a.monasterio.a@gmail.com>
2020-04-30 21:24:04 -0500

    Fix navbar slider size and add classnames to ColorBox

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -12,8 +12,12 @@ class MiniPalette extends PureComponent {
     e.stopPropagation();
     this.props.openDialog(this.props.id);
   }
+
+  handleClick(){
+    this.props.goToPalette(this.props.id)
+  }
   render() {
-    const { classes, paletteName, emoji, colors, handleClick } = this.props;
+    const { classes, paletteName, emoji, colors} = this.props;
     const miniColorBoxes = colors.map(color => (
       <div
         className={classes.miniColor}
@@ -22,7 +26,7 @@ class MiniPalette extends PureComponent {
       />
     ));
     return (
-      <div className={classes.root} onClick={handleClick}>
+      <div className={classes.root} onClick={this.handleClick}>
         <DeleteIcon
           className={classes.deleteIcon}
           style={{ transition: "all 0.3s ease-in-out" }}
